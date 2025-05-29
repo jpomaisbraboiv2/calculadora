ef4ea53daaac93f20e4ad651e9432fe3ab41f96a
João Pedro <joao.resende@ivee.tech>
2025-05-29 10:59:30 -0300

    fix: fix operators sequence bugs and negative numbers feature

--- index.js
+++ index.js
@@ -3,37 +3,59 @@ const keys = document.querySelector(".keys")
 
 
 const updateResult = (value) => {
-    const operators = ['+', '-', '*', '/', '.', 'X']
+    const operators = ['+', '*', '/', 'X', '-']
+    const lastChar = result.value.slice(-1)
 
     if (value === 'CE') {
-        return result.value = '0'
+        result.value = '0'
+        return
     }
 
     if (value === '=') {
-        return result.value = eval(result.value.replace(/X/g, '*'))
+        if (operators.includes(lastChar) || lastChar === '.') {
+            return
+        }
+        result.value = eval(result.value.replace(/X/g, '*'))
+        return
     }
 
-    if (result.value === '0' && value !== '0' && !operators.includes(value)) {
+    if (result.value === '0') {
+        if (operators.includes(value) && value !== '-') {
+            return
+        }
+        if (value === '-') {
+            return result.value = '-'
+
+        }
+        if (value === '0') {
+            return
+        }
         result.value = ''
     }
 
-    const lastChar = result.value.slice(-1);
-
-    if (operators.includes(lastChar) && operators.includes(value)) {
-        return;
-    }
-
     if (value === '.') {
-        const parts = result.value.split(/[\+\-\*\/X]/);
-        const currentNumber = parts[parts.length - 1];
-
+        const parts = result.value.split(/[\+\-\*\/X]/)
+        const currentNumber = parts[parts.length - 1]
         if (currentNumber.includes('.')) {
             return
         }
     }
 
-    return result.value += value
+    if (operators.includes(lastChar) && operators.includes(value)) {
+        if (value === '-') {
+            if (lastChar !== '-') {
+                return result.value += value
+
+            }
+            return
+        }
+
+        return
+    }
+
+    result.value += value
 }
+
 
 keys.addEventListener('click', e => {
     const key = e.target.textContent
